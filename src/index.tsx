ec922d28acef0162b17029af089fb87b64efdabd
Luke Joliat <luke.joliat@S02079-MBPR.local>
2020-07-06 14:59:03 -0400

    add assets

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+interface AppProps { }
+interface AppState {
+  name: string;
+}
+
+class App extends Component<AppProps, AppState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return null;
+  }
+}
+
+render(<App />, document.getElementById('root'));
